ecaf4451fe3a630c4433acd97d1bbb69f5871aec
Smart developer <lastik338@gmail.com>
2025-04-10 18:31:35 +0300

    школьный проект

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 from flask_socketio import SocketIO, emit
 import io
 import base64
@@ -8,7 +8,18 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key_123'
 socketio = SocketIO(app)
-n = 13
+zoom = 15
+CITY = ['Кейптаун', 'Москва', 'Нью-Йорк', 'Париж', 'Сидней', 'Токио']
+COORDS = [
+    [-33.9249, 18.4241],
+    [-33.8688, 151.2093],
+    [55.7558, 37.6176],
+    [40.7128, -74.0060],
+    [48.8566, 2.3522],
+    [35.6762, 139.6503]
+]
+city_num = -1
+lat, lon = 0, 0
 
 
 def get_cords(geocode):
@@ -41,44 +52,70 @@ def get_response(point, zoom):
 
 
 # Функция для генерации случайного изображения (цветной квадрат)
-def generate_map_image(zoom):
-    city = ['Москва']
-
-    cords = [
-        get_response(get_cords(i), str(zoom)) for i in city
-    ]
-    img_str = base64.b64encode(cords[0].content).decode("utf-8")
+def generate_map_image(zoom, image):
+    with open(image, "rb") as file:
+        binary_data = file.read()
+    img_str = base64.b64encode(binary_data).decode("utf-8")
     return img_str
 
 
 # Главная страница
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', fon=url_for('static', filename='fon.png'))
 
 
 # Обработчик WebSocket-запроса на обновление изображения
 @socketio.on('request_plus_image')
 def handle_image_request():
-    global n
-    img_data = generate_map_image(n)
+    global zoom, CITY, city_num
+    img_data = generate_map_image(zoom, f'static/{zoom}/{CITY[city_num]}.png')
     emit('update_image', {'image_data': img_data})
-    n += 1
+    zoom += 1
+    if zoom < 17:
+        zoom += 1
+    else:
+        zoom = 17
 
 
 @socketio.on('request_dif_image')
 def handle_image_request():
-    global n
-    img_data = generate_map_image(n)
+    global zoom, CITY, city_num
+    img_data = generate_map_image(zoom, f'static/{zoom}/{CITY[city_num]}.png')
     emit('update_image', {'image_data': img_data})
-    n -= 1
+    if zoom > 12:
+        zoom -= 1
+    else:
+        zoom = 12
 
 
 @socketio.on('marker_position')
 def handle_marker_position(data):
+    global lat, lon
     lat = data['latitude']
     lon = data['longitude']
-    print(f"Новые координаты метки: {lat}, {lon}")
+
+
+@socketio.on('result_map')
+def handle_marker_position():
+    global lat, lon, city_num, COORDS
+    if city_num != 5:
+        city_num += 1
+    else:
+        city_num = 0
+    print(COORDS[city_num][0], COORDS[city_num][1])
+    emit('redirect_to_result', {'a': lat, 'b': lon})
+
+
+@app.route('/result')
+def result():
+    global COORDS
+    a = request.args.get('a')
+    b = request.args.get('b')
+    c = str(COORDS[city_num][0])
+    d = str(COORDS[city_num][1])
+    return render_template('result.html', fon=url_for('static', filename='fon.png'),
+                           user_cords='[' + a + ',' + b + ']', true_cords='[' + c + ',' + d + ']')
 
 
 if __name__ == '__main__':
